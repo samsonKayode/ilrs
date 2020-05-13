752d77ff55aa70b41f09f235b014bb5ff3bb41f7
kayode <kayode@kayodes-mbp>
2020-05-13 14:59:39 +0100

    base configuration push

---
+++ ImoLandRegistry/src/main/java/com/imo/landregistry/ImoLandRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.imo.landregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoLandRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoLandRegistryApplication.class, args);
+	}
+
+}
